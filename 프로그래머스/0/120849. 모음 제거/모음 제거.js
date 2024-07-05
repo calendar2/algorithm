c8e52f8ef936db08ccbda1b7d8406ae31d4f657e
차민재 <122072805+calendar2@users.noreply.github.com>
2024-07-05 12:29:34 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,22 @@
+function solution(my_string) {
+    var answer = '';
+    
+    for (const letter of my_string) {
+        switch (letter) {
+            case 'a' :
+                break;
+            case 'e' :
+                break;
+            case 'i' :
+                break;
+            case 'o' :
+                break;
+            case 'u' :
+                break;
+            default :
+                answer += letter;
+        }
+    }
+    
+    return answer;
+}
