0945340f926bdd37cf675a123f20a5be53978f49
차민재 <122072805+calendar2@users.noreply.github.com>
2024-07-09 22:56:16 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.82 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -0,0 +1,20 @@
+class Solution {
+    static private int mod = 10007;
+    
+    public long solution(int n, int[] tops) {
+        long[][] dp = new long[n+1][2];
+        dp[0][1] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            dp[i+1][0] = (dp[i][0] + dp[i][1]) % mod;
+            
+            if (tops[i] == 0) {
+                dp[i+1][1] = (dp[i][0] + dp[i][1] * 2) % mod;
+            } else {
+                dp[i+1][1] = (dp[i][0] * 2 + dp[i][1] * 3) % mod;
+            }
+        }
+
+        return (dp[n][0] + dp[n][1]) % mod;
+    }
+}
