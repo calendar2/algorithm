ff561dd840f683c19008f2f9d2dd3ca513557a8c
차민재 <122072805+calendar2@users.noreply.github.com>
2024-07-30 13:09:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+function solution(a, b, c) {
+    var answer = 0;
+    
+    if (a === b && b === c) {
+        answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+    } else if (a === b || b === c || c === a) {
+        answer = (a + b + c) * (a*a + b*b + c*c);
+    } else {
+        answer = a + b + c;
+    }
+    
+    return answer;
+}
