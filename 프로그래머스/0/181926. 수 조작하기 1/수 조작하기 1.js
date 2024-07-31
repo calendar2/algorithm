6b9b0abb060ed80ff47c89b95516b30c5de645cd
차민재 <122072805+calendar2@users.noreply.github.com>
2024-07-31 15:06:06 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.00 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    for (const c of control) {
+        if (c === 'w') {
+            n++;
+        } else if (c === 's') {
+            n--;
+        } else if (c === 'd') {
+            n += 10;
+        } else if (c === 'a') {
+            n -= 10;
+        }
+    }
+    
+    return n;
+}
