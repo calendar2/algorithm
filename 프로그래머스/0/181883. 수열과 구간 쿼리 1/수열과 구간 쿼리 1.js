9f7ae7a85977e0352fbb8062ab930e144e551f53
차민재 <122072805+calendar2@users.noreply.github.com>
2024-08-20 12:05:39 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.76 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    for (const query of queries) {
+        for (let i = query[0]; i <= query[1]; i++) {
+            arr[i]++;
+        }
+    }
+    
+    return arr;
+}
