733072f99101046e4f7a923b4163a66b5013d254
차민재 <122072805+calendar2@users.noreply.github.com>
2024-08-26 12:55:47 +0900

    [level 0] Title: ad 제거하기, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,11 @@
+function solution(strArr) {
+    var answer = [];
+    
+    for (const str of strArr) {
+        if (!str.includes('ad')) {
+            answer.push(str);
+        }
+    }
+    
+    return answer;
+}
