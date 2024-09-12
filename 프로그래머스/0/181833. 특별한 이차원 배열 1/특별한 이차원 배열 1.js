d9ef41690168acb363e200ba67de0e2c437afc95
차민재 <122072805+calendar2@users.noreply.github.com>
2024-09-12 12:46:47 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.13 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        const temp = new Array(n).fill(0);
+        temp[i] = 1;
+        answer.push(temp);
+    }
+    
+    return answer;
+}
