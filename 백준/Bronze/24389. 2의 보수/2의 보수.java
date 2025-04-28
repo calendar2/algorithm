e62099b432c22aa28b774eab9281d0219414879e
차민재 <122072805+calendar2@users.noreply.github.com>
2025-04-28 22:04:22 +0900

    [Bronze I] Title: 2의 보수, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/24389. 2의 보수/2의 보수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] two_path = new int[32];
+        for (int i = 31; i >= 0; i--) {
+            two_path[i] = N % 2;
+            N /= 2;
+        }
+
+        int[] reverse_two_path = new int[32];
+        for (int i = 0; i < 32; i++) {
+            if (two_path[i] == 0) {
+                reverse_two_path[i] = 1;
+            } else {
+                reverse_two_path[i] = 0;
+            }
+        }
+
+        int bit = 31;
+        while (bit >= 0) {
+            if (reverse_two_path[bit] == 0) {
+                reverse_two_path[bit] = 1;
+                break;
+            } else {
+                reverse_two_path[bit] = 0;
+                bit -= 1;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if (two_path[i] != reverse_two_path[i]) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
