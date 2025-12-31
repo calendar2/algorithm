84162ec47de7020dc969f7c017158bedcadf2dd6
차민재 <122072805+calendar2@users.noreply.github.com>
2026-01-01 00:17:58 +0900

    [Silver I] Title: 점프, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] board = new int[N][N];
+		long[][] dp = new long[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		dp[0][0] = 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				int jump = board[i][j];
+
+				if (jump == 0) {
+					break;
+				}
+
+				if (j + jump < N) {
+					dp[i][j+jump] += dp[i][j];
+				}
+
+				if (i + jump < N) {
+					dp[i+jump][j] += dp[i][j];
+				}
+			}
+		}
+
+		System.out.println(dp[N-1][N-1]);
+	}
+}
